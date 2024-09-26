b679c1d67d80b3f12e73a6460e1ee2d65082d512
mchen610 <melvinchen610@gmail.com>
2024-09-26 18:53:03 -0400

    new template without nx

--- apps/frontend/eslint.config.js
+++ apps/frontend/eslint.config.js
@@ -1,12 +1,28 @@
-const nx = require("@nx/eslint-plugin");
-const baseConfig = require("../../eslint.config.js");
+import js from '@eslint/js'
+import globals from 'globals'
+import reactHooks from 'eslint-plugin-react-hooks'
+import reactRefresh from 'eslint-plugin-react-refresh'
+import tseslint from 'typescript-eslint'
 
-module.exports = [
-  ...baseConfig,
-  ...nx.configs["flat/react"],
+export default tseslint.config(
+  { ignores: ['dist'] },
   {
-    files: ["**/*.ts", "**/*.tsx", "**/*.js", "**/*.jsx"],
-    // Override or add rules here
-    rules: {},
+    extends: [js.configs.recommended, ...tseslint.configs.recommended],
+    files: ['**/*.{ts,tsx}'],
+    languageOptions: {
+      ecmaVersion: 2020,
+      globals: globals.browser,
+    },
+    plugins: {
+      'react-hooks': reactHooks,
+      'react-refresh': reactRefresh,
+    },
+    rules: {
+      ...reactHooks.configs.recommended.rules,
+      'react-refresh/only-export-components': [
+        'warn',
+        { allowConstantExport: true },
+      ],
+    },
   },
-];
+)
--- apps/frontend/src/main.tsx
+++ apps/frontend/src/main.tsx
@@ -1,13 +1,10 @@
-import { StrictMode } from "react";
-import * as ReactDOM from "react-dom/client";
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import App from './App.tsx'
+import './index.css'
 
-import App from "./app/app";
-
-const root = ReactDOM.createRoot(
-  document.getElementById("root") as HTMLElement
-);
-root.render(
+createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
-  </StrictMode>
-);
+  </StrictMode>,
+)
