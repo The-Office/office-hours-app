d17dc7eb3d26111558b052dccc5402e8d3b75c26
Justin Brown <105444355+macronym@users.noreply.github.com>
2024-10-12 19:21:44 -0400

    Change text size and hide image when on smaller screens

--- apps/frontend/src/components/hero-section.tsx
+++ apps/frontend/src/components/hero-section.tsx
@@ -7,12 +7,12 @@ import clock_gray_cropped from '../assets/clock-gray-wall-cropped.jpg';
 
 const HeroSection = () => {
     return (
-        <div className="hero-section text-black"> {/* Added pt-12 for padding-top */}
+        <div className="hero-section text-black">
             <div className="flex flex-col md:flex-row h-full">
-                <div className="w-full md:w-1/2 flex items-center justify-center h-full">
+                <div className="w-full md:w-1/2 flex items-center justify-center h-screen">
                     <div className="text-center">
-                        <h2 className="text-6xl font-bold mb-4">OFFICE HOURS<br></br>MADE EASY</h2>
-                        <h3 className="text-2xl mb-4 font-semibold">one import. automatic updates.</h3>
+                        <h2 className="text-5xl md:text-6xl font-bold mb-4">OFFICE HOURS<br></br>MADE EASY</h2>
+                        <h3 className="text-xl md:text-2xl mb-4 font-semibold">one import. automatic updates.</h3>
                         <div className="flex justify-center">
                             <button className="shadow-lg bg-white px-3 py-2 rounded-xl flex items-center hover:bg-gray-50 hover:shadow-md">
                                 <img src={canvas_logo} alt="canvas logo" className="w-10 h-10 mr-2" />
@@ -21,7 +21,7 @@ const HeroSection = () => {
                         </div>
                     </div>
                 </div>
-                <div className="w-full md:w-1/2 flex items-center justify-center h-screen">
+                <div className="hidden md:block w-full md:w-1/2 items-center justify-center">
                     <img src={clock_gray_cropped} alt="picture of calendar and clock" className="w-full h-full object-cover" />
                 </div>
             </div>
