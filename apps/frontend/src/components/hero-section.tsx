b9d8045d22c6e12122b7c8c31c9d206d5e407847
Justin Brown <105444355+macronym@users.noreply.github.com>
2024-10-12 03:19:26 -0400

    Increase text size and styling closer to the Figma mockup

--- apps/frontend/src/components/hero-section.tsx
+++ apps/frontend/src/components/hero-section.tsx
@@ -11,12 +11,12 @@ const HeroSection = () => {
             <div className="flex flex-col md:flex-row h-full">
                 <div className="w-full md:w-1/2 flex items-center justify-center h-full">
                     <div className="text-center">
-                        <h2 className="text-4xl font-bold mb-4">OFFICE HOURS <br></br> MADE EASY</h2>
-                        <h3 className="text-2md mb-4 font-bold">one import. automatic updates.</h3>
+                        <h2 className="text-6xl font-bold mb-4">OFFICE HOURS<br></br>MADE EASY</h2>
+                        <h3 className="text-2xl mb-4 font-semibold">one import. automatic updates.</h3>
                         <div className="flex justify-center">
                             <button className="shadow-lg bg-white px-3 py-2 rounded-xl flex items-center hover:bg-gray-50 hover:shadow-md">
-                                <img src={canvas_logo} alt="canvas logo" className="w-8 h-8 mr-2" />
-                                <span className="border-gray-300 text-gray-400 border-l-2 pl-2">LOGIN WITH CANVAS</span>
+                                <img src={canvas_logo} alt="canvas logo" className="w-10 h-10 mr-2" />
+                                <span className="border-gray-300 text-gray-400 border-l-2 pl-2 text-xl">LOGIN WITH CANVAS</span>
                             </button>
                         </div>
                     </div>
