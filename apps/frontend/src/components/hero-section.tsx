ce6e6976dbffa3b3f7072ce11af57b8cf656ee10
Justin Brown <105444355+macronym@users.noreply.github.com>
2024-10-16 22:35:48 -0400

    Dunno what happened but fixed this

--- apps/frontend/src/components/hero-section.tsx
+++ apps/frontend/src/components/hero-section.tsx
@@ -1,18 +1,14 @@
 import canvas_logo from '../assets/canvas-logo.png';
-// import hero_image from '../assets/hero-image.webp';
-// import clock_light from '../assets/clock-light.jpg';
-// import calendar from '../assets/calendar.jpg';
-// import clock_gray from '../assets/clock-gray-wall.jpg';
 import clock_gray_cropped from '../assets/clock-gray-wall-cropped.jpg';
 
 const HeroSection = () => {
     return (
         <div className="hero-section text-black">
-            <div className="flex flex-col md:flex-row h-full">
+            <div className="flex flex-col md:flex-row h-screen">
                 <div className="w-full md:w-1/2 flex items-center justify-center h-screen">
-                    <div className="text-center">
-                        <h2 className="text-5xl md:text-6xl font-bold mb-4">OFFICE HOURS<br></br>MADE EASY</h2>
-                        <h3 className="text-xl md:text-2xl mb-4 font-semibold">one import. automatic updates.</h3>
+                    <div className="text-center space-y-4 mx-4">
+                        <h2 className="text-5xl md:text-6xl font-bold">OFFICE HOURS<br></br>MADE EASY</h2>
+                        <h3 className="text-xl md:text-2xl font-semibold">one import. automatic updates.</h3>
                         <div className="flex justify-center">
                             <button className="shadow-lg bg-white px-3 py-2 rounded-xl flex items-center hover:bg-gray-50 hover:shadow-md">
                                 <img src={canvas_logo} alt="canvas logo" className="w-10 h-10 mr-2" />
