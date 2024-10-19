c880b07c944e075c568f4a9141d81e9632ab420d
tmillner21 <tmillner@ufl.edu>
2024-10-19 16:22:15 -0400

    Fixed .SQL file syntax

--- apps/backend/src/database/migrations/sql/2_create_office_hours_table_up.sql
+++ apps/backend/src/database/migrations/sql/2_create_office_hours_table_up.sql
@@ -1,11 +1,13 @@
 CREATE TABLE IF NOT EXISTS office_hours (
-  course_id INT FOREIGN KEY,
+  course_id INT,
   course_code VARCHAR(255) NOT NULL,
-  office_hour_provider_id INT FOREIGN KEY,
+  office_hour_provider_id INT,
   modality VARCHAR(255),
   office_hour_location VARCHAR(255),
   start_time VARCHAR(255),
   end_time VARCHAR(255),
   created_at DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  updated_at DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+  updated_at DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  FOREIGN KEY (course_id) REFERENCES courses(course_id), 
+  FOREIGN KEY (office_hour_provider_id) REFERENCES users(id) 
 );
