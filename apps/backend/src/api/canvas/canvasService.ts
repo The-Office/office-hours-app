e22e2c6e8291bfa339d425ac72b0ea11dd0224cd
mchen610 <melvinchen610@gmail.com>
2024-10-20 15:12:01 -0400

    update ype

--- apps/backend/src/api/canvas/canvasService.ts
+++ apps/backend/src/api/canvas/canvasService.ts
@@ -1,36 +1,74 @@
 import axios from 'axios'
 // Using this video as tutorial template [https://www.youtube.com/watch?v=g-XOxZxrW6Q]
 
-export type Courses = {
-    list: string;   // Json data
-}
+interface Course {
+    id: number;
+    name: string;
+    account_id: number;
+    uuid: string;
+    start_at: string | null;
+    grading_standard_id: number | null;
+    is_public: boolean;
+    created_at: string;
+    course_code: string;
+    default_view: string;
+    root_account_id: number;
+    enrollment_term_id: number;
+    license: string;
+    grade_passback_setting: string | null;
+    end_at: string | null;
+    public_syllabus: boolean;
+    public_syllabus_to_auth: boolean;
+    storage_quota_mb: number;
+    is_public_to_auth_users: boolean;
+    homeroom_course: boolean;
+    course_color: string | null;
+    friendly_name: string | null;
+    apply_assignment_group_weights: boolean;
+    calendar: {
+      ics: string;
+    };
+    time_zone: string;
+    blueprint: boolean;
+    template: boolean;
+    enrollments: Enrollment[];
+    hide_final_grades: boolean;
+    workflow_state: string;
+    restrict_enrollments_to_course_dates: boolean;
+  }
+  
+  interface Enrollment {
+    type: string;
+    role: string;
+    role_id: number;
+    user_id: number;
+    enrollment_state: string;
+    limit_privileges_to_course_section: boolean;
+  }
 
-export type Files = {
-    file_list: string; // Json file data?
-}
+  interface File {}
+  
 
-export async function getCourses(accessToken: string): Promise<Courses> {
+export async function getCourses(accessToken: string): Promise<Course[]> {
     const url = `https://ufl.instructure.com/api/v1/courses/`;
     const headers = {
         'Authorization': `Bearer ${accessToken}`
     }
     const res = await axios.get(url, {
-        method: 'GET',  // or 'POST', 'PUT', etc. if needed
-        headers: headers
+        headers
     });
-    return { list: res.data }
 
+    return res.data as Course[];
 }
 
-export async function getCourseFiles(course_id: string, accessToken: string): Promise<Files> {
+export async function getCourseFiles(course_id: string, accessToken: string): Promise<File[]> {
     const url = `https://ufl.instructure.com/api/v1/courses/${course_id}/files`
 
     const headers = {
         'Authorization': `Bearer ${accessToken}`
     }
     const res = await axios.get(url, {
-        method: 'GET',
-        headers: headers
+        headers
     })
-    return { file_list: res.data }
+    return res.data as File[]
 }
