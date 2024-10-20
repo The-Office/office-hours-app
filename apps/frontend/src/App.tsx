0b8b89549b95c4d00cbb7f0c903572844ca05e3d
Justin Brown <105444355+macronym@users.noreply.github.com>
2024-10-20 14:53:23 -0400

    non working code

--- apps/frontend/src/App.tsx
+++ apps/frontend/src/App.tsx
@@ -1,6 +1,8 @@
 import LandingPage from './components/landing-page.tsx';
 import Dashboard from './components/dashboard.tsx';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { Suspense, lazy } from 'react';
+import DemoPage from './app/payments/page.tsx';
 
 const App = () => {
   return (
@@ -9,6 +11,7 @@ const App = () => {
         <Routes>
           <Route path="/" element={<LandingPage />} />
           <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="/demo" element={<DemoPage />} />
         </Routes>
       </Router>
     </>
