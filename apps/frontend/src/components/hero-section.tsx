3a80b4d45d70122308f999676feb8ae919989921
Justin Brown <105444355+macronym@users.noreply.github.com>
2024-10-19 23:55:05 -0400

    Add better text size and responsiveness

--- apps/frontend/src/components/hero-section.tsx
+++ apps/frontend/src/components/hero-section.tsx
@@ -6,13 +6,13 @@ const HeroSection = () => {
         <div className="hero-section text-black">
             <div className="flex flex-col md:flex-row h-screen">
                 <div className="w-full md:w-1/2 flex items-center justify-center h-screen">
-                    <div className="text-center space-y-4 mx-4">
-                        <h2 className="text-5xl md:text-6xl font-bold">OFFICE HOURS<br></br>MADE EASY</h2>
-                        <h3 className="text-xl md:text-2xl font-semibold">one import. automatic updates.</h3>
+                    <div className="text-center">
+                        <h2 className="text-5xl md:text-6xl lg:text-7xl font-bold mb-4">OFFICE HOURS<br></br>MADE EASY</h2>
+                        <h3 className="text-xl md:text-2xl mb-4 font-semibold">one import. automatic updates.</h3>
                         <div className="flex justify-center">
                             <button className="shadow-lg bg-white px-3 py-2 rounded-xl flex items-center hover:bg-gray-50 hover:shadow-md">
                                 <img src={canvas_logo} alt="canvas logo" className="w-10 h-10 mr-2" />
-                                <span className="border-gray-300 text-gray-400 border-l-2 pl-2 text-xl">LOGIN WITH CANVAS</span>
+                                <span className="border-gray-300 text-gray-400 border-l-2 pl-2 text-2xl">LOGIN WITH CANVAS</span>
                             </button>
                         </div>
                     </div>
