390f5a247bdb34c2a573af3908c3d3141330e113
Justin Brown <105444355+macronym@users.noreply.github.com>
2024-11-17 00:13:07 -0500

    Add checkboxes for row selection on /dashboard

--- apps/frontend/src/components/table/columns.tsx
+++ apps/frontend/src/components/table/columns.tsx
@@ -3,9 +3,32 @@
 import { ColumnDef } from "@tanstack/react-table"
 import { DataTableColumnHeader } from "@/components/ui/table-column-headers"
 import { OfficeHour } from "@/services/userService"
+import { Checkbox } from "@/components/ui/checkbox"
 
 
 export const columns: ColumnDef<OfficeHour>[] = [
+  {
+    id: "select",
+    header: ({ table }) => (
+      <Checkbox
+        checked={
+          table.getIsAllPageRowsSelected() ||
+          (table.getIsSomePageRowsSelected() && "indeterminate")
+        }
+        onCheckedChange={(value) => table.toggleAllPageRowsSelected(!!value)}
+        aria-label="Select all"
+      />
+    ),
+    cell: ({ row }) => (
+      <Checkbox
+        checked={row.getIsSelected()}
+        onCheckedChange={(value) => row.toggleSelected(!!value)}
+        aria-label="Select row"
+      />
+    ),
+    enableSorting: false,
+    enableHiding: false,
+  },
   {
     accessorKey: "course_code",
     header: ({ column }) => (
--- apps/frontend/src/components/table/data-table.tsx
+++ apps/frontend/src/components/table/data-table.tsx
@@ -39,8 +39,8 @@ export function DataTable<TData, TValue>({
 }: DataTableProps<TData, TValue>) {
     const [sorting, setSorting] = React.useState<SortingState>([])
     const [columnFilters, setColumnFilters] = React.useState<ColumnFiltersState>([])
-
     const [columnVisibility, setColumnVisibility] = React.useState<VisibilityState>({})
+    const [rowSelection, setRowSelection] = React.useState({})
 
     const table = useReactTable({
         data,
@@ -52,10 +52,12 @@ export function DataTable<TData, TValue>({
         onColumnFiltersChange: setColumnFilters,
         getFilteredRowModel: getFilteredRowModel(),
         onColumnVisibilityChange: setColumnVisibility,
+        onRowSelectionChange: setRowSelection,
         state: {
             sorting,
             columnFilters,
             columnVisibility,
+            rowSelection,
         },
     })
 
