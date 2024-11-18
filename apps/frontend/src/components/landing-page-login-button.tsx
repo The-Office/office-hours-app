8c1a88b8b031eae87d4edd8870ac752cb27babcd
Justin Brown <105444355+macronym@users.noreply.github.com>
2024-11-17 21:24:01 -0500

    Remove unecessary canvas image comment from old iteration

--- apps/frontend/src/components/landing-page-login-button.tsx
+++ apps/frontend/src/components/landing-page-login-button.tsx
@@ -6,7 +6,6 @@ export default function LoginButton() {
             <SignedOut>
                 <SignInButton mode="modal" forceRedirectUrl="/dashboard">
                     <button className="shadow-lg bg-red-500 px-3 py-2 rounded-xl flex items-center hover:bg-red-400 hover:shadow-md">
-                        {/* <img src={canvas_logo} alt="canvas logo" className="w-10 h-10 mr-2" /> */}
                         <span className="border-gray-300 text-white text-2xl">SIGN IN TO GET STARTED</span>
                     </button>
                 </SignInButton>
